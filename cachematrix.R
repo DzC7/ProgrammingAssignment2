3b9b1e913fc5c77e805ce20f43c86b8d3bfd3178
David Costar <dzcostar@gmail.com>
2022-11-04 12:22:55 +1100

    Assignment for GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This will create a function that creates a matrix object that caches its 
+## inverse
+## Using the example of 'Caching a Mean of a Vector' I simply substituted the 
+## 'mean' functions with 'solve'
+## 'iv' is the input  
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) ##setting matrix to default 
+{
+  iv <- NULL
+  set <- function(y) {
+    x <<- y
+    iv <<- NULL ##setting solved value to NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) iv <<- solve 
+  ##replaced the 'means' with 'solved'
+  getsolve <- function() iv ##replace 'get/setmean' to 'get/setsolve
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+##This function computes the inverse of the matrix returned by 'makeCacheMatrix'
+## From example, converted all 'getmean/setmean' to 'getsolve/setsolve'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  iv <- x$getsolve()
+  if(!is.null(iv)) {
+    message("getting cached data")
+    return(iv)
+  }
+  data <- x$get()
+  iv <- solve(data, ...)
+  x$setsolve(iv)
+  iv
 }
